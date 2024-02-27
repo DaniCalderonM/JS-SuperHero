92dcfd01b8b55f8d9863f5470e63c4cdcde5551a
DaniCalderon <mesiasd.calderon@gmail.com>
2024-02-27 11:18:45 -0300

    mejorando logica

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ window.onload = function () {
                 alert('Por favor, ingrese solo números.');
                 return;
             }
-            
+
             //Creamos el else if para informar cuando la ID ingresada no tiene datos de
             //un superheroe porque esta fuera del rango
             else if ((idSuperHero) > 732 || (idSuperHero) == 0) {
@@ -34,7 +34,7 @@ window.onload = function () {
             //el h2 y la card
             $('h2').show();
             $('.card').show();
-            
+
             //Creamos la peticion AJAX con un GET sobre la Api para conectarnos y extraer la informacion
             $.ajax({
                 type: "GET",
@@ -43,15 +43,15 @@ window.onload = function () {
                 url: `https://www.superheroapi.com/api.php/4905856019427443/${idSuperHero}`,
                 dataType: "json",
                 success: function (datos) {
-                     //Le asignamos datos.powerstats a datosGrafico para que nos
+                    //Le asignamos datos.powerstats a datosGrafico para que nos
                     //muestre sus valores posteriormente
                     let datosGrafico = datos.powerstats;
-                     //Mostramos en consola lo que nos devuelve datos
+                    //Mostramos en consola lo que nos devuelve datos
                     console.log("Success valor de datos obtenidos: ", datos);
                     //Mostramos en consola lo que nos devuelve datosGrafico
                     console.log("Success valor de datosGrafico obtenidos: ", datosGrafico);
-                    
-                    // Creamos la variable para saber si se encontraron datos en las powerstats
+                    // Creamos la variable para luego verificar si se encontraron datos
+                    //null o numericos en las powerstats
                     let encontrarDatos = false;
                     //Creamos un for in para recorrer los datos de los powerstats
                     //y se lo añadimos a dataPoints con un .push
@@ -60,26 +60,31 @@ window.onload = function () {
                         //entregados en las powerstats
                         let numero = parseInt(datosGrafico[resultado]);
                         //Creamos la logica para verificar si el dato entregado es 
-                        //es un numero valido.
-                        if (!isNaN(numero)) {
+                        //es un numero valido y no "null".
+                        if (isNaN(numero)) {
+                            //Aqui indicamos si se encontraron datos no numericos
+                            //como null, que se cree el grafico devolviendo en el label la leyenda
+                            //`${resultado}= Sin datos` (resultado= powerstat correspondiente) y 
+                            //entregamos un numero cualquiera en el eje y, en este caso un 1 para que
+                            //nos muestre el grafico, ya que si añadimos 0, no nos grafica el powerstat 
+                            //y en caso de que algun superheroe tenga todo "null", no nos grafica nada.
+                            dataPoints.push({
+                                label: `${resultado}= Sin datos`,
+                                y: 1
+                            });
+                        }
+                        else {
                             //Aqui indicamos si se encontraron datos, que se cree el grafico
-                            encontrarDatos = true; 
+                            //correctamente
+                            encontrarDatos = true;
                             dataPoints.push({
                                 label: resultado,
                                 y: numero
                             });
                         }
                     }
-                    // Si no se encontraron datos, mostramos un mensaje distinto en el
-                    //label y le asignamos un valor cualquiera, en este caso un 1 al eje y,
-                    //para que nos muestre el grafico, ya que si ponemos 0, no nos dibuja nada.
-                    if (!encontrarDatos) {
-                        dataPoints.push({
-                            label: "Dato no disponible",
-                            y: 1
-                        });
-                    }
-                 //Creamos el objeto que nos pide Canvas JS para hacer el grafico
+
+                    //Creamos el objeto que nos pide Canvas JS para hacer el grafico
                     let options = {
                         title: { text: `Estadísticas de Poder para ${datos.name}` },
                         data: [
@@ -89,7 +94,7 @@ window.onload = function () {
                             }
                         ]
                     };
-                     //Indicamos donde ira el grafico (#chartContainer) y llamamos a un
+                    //Indicamos donde ira el grafico (#chartContainer) y llamamos a un
                     //plugin externo para crear el grafico con los valores de options
                     $("#chartContainer").CanvasJSChart(options);
                     //Asignamos los valores a la CARD en el html
